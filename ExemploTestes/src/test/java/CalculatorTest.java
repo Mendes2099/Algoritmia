c5e129d78b33c99869be991979cbb97bcce651de
João Mendes <mendes19966@gmail.com>
2023-06-06 12:56:42 +0100

    .

--- ExemploTestes/src/test/java/CalculatorTest.java
+++ ExemploTestes/src/test/java/CalculatorTest.java
@@ -9,11 +9,23 @@ public class CalculatorTest {
     public void setUp() {
         calculator = new Calculator();
     }
-
+    
     @Test
     public void testAdd() {
         int result = calculator.add(2, 3);
         assertEquals(5, result);
+    }
+
+    @Test
+    public void multiply() {
+        int result = calculator.multiply(2,2);
+        assertEquals(4,result);
+    }
+
+    @Test
+    public void subtract() {
+        int result = calculator.subtract(2,1);
+        assertEquals(1,result);
     }
 
     @Test
