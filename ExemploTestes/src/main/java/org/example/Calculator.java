4f8d4d83369a11e3a3b8ea8b5a3c1fcb924a62d2
João Mendes <mendes19966@gmail.com>
2023-06-06 12:39:18 +0100

    .

--- ExemploTestes/src/main/java/org/example/Calculator.java
+++ ExemploTestes/src/main/java/org/example/Calculator.java
@@ -1,6 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        int num1, num2;
+        System.out.print("Primeiro número: ");
+        num1= input.nextInt();
+        System.out.print("Segundo número: ");
+        num2= input.nextInt();
+
+        System.out.println("Divisão: "+ calculator.divide(num1, num2));
+
+    }
+
     public int add(int a, int b) {
         return a + b;
     }
